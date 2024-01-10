b40367ffa148038a2e3bcf8cde5610e12580a40a
Ryan Goree <goree.ryan@gmail.com>
2024-01-10 07:35:44 -0600

    Fix image paths

--- src/utils/formatPath.ts
+++ src/utils/formatPath.ts
@@ -1,3 +1,6 @@
 export function formatPath(path: string) {
-  return `${import.meta.env.BASE_URL}${path.replace(/^\//, "")}`;
+  return `${import.meta.env.BASE_URL.replace(/\/$/, "")}/${path.replace(
+    /^\//,
+    "",
+  )}`;
 }
