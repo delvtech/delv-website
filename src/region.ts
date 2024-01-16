41022ccf4123a37e2df7205d9d6f09434cf294e5
Ryan Goree <goree.ryan@gmail.com>
2024-01-16 14:08:15 -0600

    Change region names

--- src/region.ts
+++ src/region.ts
@@ -1,3 +1,3 @@
-export const regions = ["us", "eu"] as const;
+export const regions = ["default", "uk"] as const;
 export type Region = (typeof regions)[number];
 export const region: Region = import.meta.env.VITE_REGION;
